35e735e4513172d8bb18e9769cc5396dd88934de
Aroa Mendez <aroazmendez@gmail.com>
2023-02-25 18:43:41 +0100

    Añadir ejercicios

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ProyectoFinal-ShowcaseDinamico-GeeksHubs
+Ejercicio correspondiente al bootcamp de GeekHubs, programación desde cero.
+
+El objetivo del ejercicio es recrear un mostrador de alimentos dinámico con carrito de compra.
+
+Los alimentos se arrastran de la estanteria y se sueltan en la bolsa de la compra que se encuentra en el mostrador. La suma del precio de los productos se irá viendo en la pantalla de la maquina registradora del mostrador. 
+
+A su vez, en la parte de arriba de la pantalla se ira viendo lo que se añade en la bolsa, dando así la opción de poder volver a cogerlo para colocarlo en la estanteria descontandolo de la suma de la compra.
+
+Para realizar el ejercicio se utiliza:
+- Código JS
+- Código ES6
+
+> NOTA: La tienda está pensada para ser utilizada en pantallas de tamaño de 1920 * 1080px.
---
+++ index.js
@@ -0,0 +1,47 @@
+const priceList = {
+    cocacola: 2,
+    teVerde: 2.3,
+    teOrganico: 5.9,
+    pan: 1.1,
+    queso: 5,
+    chocolate: 7.3,
+    curasan: 4,
+    galleta: 1,
+    albaricoque: 0.6,
+    albaricoqueDocena: 3.5,
+    berenjena: 0.5,
+}
+let counter = 0;
+const convertIdToValidString = value => {
+    let str = value;
+    let position = str.indexOf('0');
+    return str.substring(0, position);
+}
+const addPriceWhenAddToBasket = product => {
+    counter += priceList[product];
+    document.getElementById('amountBasket').innerHTML = counter.toFixed(2);
+}
+const substractPriceWhenRemovedFromBasket = product => {
+    counter -= priceList[product];
+    document.getElementById('amountBasket').innerHTML = counter.toFixed(2);
+}
+const drag = ev => {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+const dropBasket = ev => {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    let item = document.getElementById(data);
+    addPriceWhenAddToBasket(convertIdToValidString(data));
+    document.getElementById("list").appendChild(item);
+}
+const dropShelf = ev => {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    let item = document.getElementById(data);
+    substractPriceWhenRemovedFromBasket(convertIdToValidString(data));
+    ev.target.appendChild(item);
+}
+const allowDrop = ev => {
+    ev.preventDefault();
+}
